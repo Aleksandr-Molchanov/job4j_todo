e3811129e939ff1e21db34ab7e515195bf40be7b
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2022-05-14 13:53:23 +0300

    4. Категории в TODO List [#331991 #298055]

--- src/main/java/ru/job4j/todo/persistence/ItemDBStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemDBStore.java
@@ -44,11 +44,11 @@ public class ItemDBStore implements DBStore {
                             + " i.categories = :categories "
                             + " where i.id = :id";
                     final Query query = session.createQuery(hql);
-                    query.setParameter("id", item.getId());
                     query.setParameter("name", item.getName());
                     query.setParameter("description", item.getDescription());
                     query.setParameter("created", item.getCreated());
                     query.setParameter("categories", item.getCategories());
+                    query.setParameter("id", item.getId());
                     System.out.println(item.getCategories());
                     int rsl = query.executeUpdate();
                     return rsl != 0;
